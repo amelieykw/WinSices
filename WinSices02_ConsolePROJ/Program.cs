9c6edef8f89b5a3aa21777fcff9bafa0bea07949
pol-yu <yu.kaiwen@etu.unice.fr>
2016-05-31 10:02:11 +0200

    Create a repository for this project by using Xamarin Studio

---
+++ WinSices02_ConsolePROJ/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace WinSices02_ConsolePROJ
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Dog dog = new Dog();
+			Console.WriteLine(dog.Describe());
+			Console.ReadKey();
+		}
+	}
+
+	abstract class FourLeggedAnimal
+	{
+		public virtual string Describe()
+		{
+			return "Not much is known about this four legged animal!";
+		}
+	}
+
+	class Dog : FourLeggedAnimal
+	{
+		public override string Describe()
+		{
+			string result = base.Describe();
+			result += " In fact, it's a dog!";
+			return result;
+		}
+	}
+}
